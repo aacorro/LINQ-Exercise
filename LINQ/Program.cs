cd303d7dcfe70e74d009c5604ad9487daac5c20c
Abdiel Corro <acorrop@outlook.com>
2022-10-19 23:59:42 -0500

    exercise 1 completed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>();
+            videoGames.Add("Tank");
+            videoGames.Add("SpaceRace");
+            videoGames.Add("Space Invaders");
+            videoGames.Add("Galaxy");
+            videoGames.Add("Pong");
+            videoGames.Add("Maze War");
+            videoGames.Add("Donkey Kong");
+            videoGames.Add("Hunt the Wumpus");
+            videoGames.Add("Colossal Cave Adventure");
+
+
+            foreach (var videoGame in videoGames.OrderBy(s => s.Length))
+            {
+                Console.WriteLine(videoGame);
+            }
+
         }
     }
 }
